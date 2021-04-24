1cd2d422bf9f0eb409b8341c29814e1be77fced4
Carlos Hernández <carloshernandez2.1b@gmail.com>
2021-04-24 15:12:12 -0500

    validate function done

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,24 +1,29 @@
 class SudokuSolver {
+  format(puzzleString) {
+    const puzzle = {},
+      puzzleGroups = puzzleString.match(/.{9}/g);
+    puzzleGroups.forEach((element, i) => {
+      puzzle[String.fromCharCode(65 + i)] = element;
+    });
+    console.log(puzzle);
+  }
 
   validate(puzzleString) {
+    const notValidCharacters = puzzleString.split("").find((element) => {
+        return !/^[\d.]$/.test(element);
+      }),
+      notEnough = puzzleString.split("").length !== 81;
+    if (notValidCharacters || notEnough) return false;
+    return true;
   }
 
-  checkRowPlacement(puzzleString, row, column, value) {
+  checkRowPlacement(puzzleString, row, column, value) {}
 
-  }
+  checkColPlacement(puzzleString, row, column, value) {}
 
-  checkColPlacement(puzzleString, row, column, value) {
+  checkRegionPlacement(puzzleString, row, column, value) {}
 
-  }
-
-  checkRegionPlacement(puzzleString, row, column, value) {
-
-  }
-
-  solve(puzzleString) {
-    
-  }
+  solve(puzzleString) {}
 }
 
 module.exports = SudokuSolver;
-
--- routes/api.js
+++ routes/api.js
@@ -1,18 +1,13 @@
-'use strict';
+"use strict";
 
-const SudokuSolver = require('../controllers/sudoku-solver.js');
+const SudokuSolver = require("../controllers/sudoku-solver.js");
 
 module.exports = function (app) {
-  
   let solver = new SudokuSolver();
 
-  app.route('/api/check')
-    .post((req, res) => {
+  app.route("/api/check").post((req, res) => {
+    console.log(solver.validate(req.body.puzzle))
+  });
 
-    });
-    
-  app.route('/api/solve')
-    .post((req, res) => {
-
-    });
+  app.route("/api/solve").post((req, res) => {});
 };
